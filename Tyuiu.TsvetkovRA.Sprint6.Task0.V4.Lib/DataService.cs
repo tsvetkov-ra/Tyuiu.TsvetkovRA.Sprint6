44146e408b7c19682c94d4190141070c3c878e7b
Admin <artykleshine@gmail.com>
2025-12-11 13:10:07 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.TsvetkovRA.Sprint6.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.TsvetkovRA.Sprint6.Task0.V4.Lib
+{
+    public class DataService : ISprint6Task0V4
+    {
+        public double Calculate(int x)
+        {
+            double res = 0;
+            res = 2.4 * Math.Pow(x, 3) + 0.4 * Math.Pow(x, 2) - 1.4 * x + 4.1;
+            return Math.Round(res, 3); 
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint6.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TsvetkovRA.Sprint6.Task0.V4.Lib;
+namespace Tyuiu.TsvetkovRA.Sprint6.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double wait = 68.3;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
