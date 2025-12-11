3312ad09303fdff9a184b80db4798145e27f6981
Admin <artykleshine@gmail.com>
2025-12-11 20:57:14 +0300

    Task3

---
+++ Tyuiu.TsvetkovRA.Sprint6.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.TsvetkovRA.Sprint6.Task3.V2.Lib
+{
+    public class DataService : ISprint6Task3V2
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (matrix[0, i] % 2 == 0)
+                {
+                    matrix[0, i] = 0;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint6.Task3.V2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.TsvetkovRA.Sprint6.Task3.V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
