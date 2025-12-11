99f68c0b726abdfab39b7a85eebd46152b3409af
Admin <artykleshine@gmail.com>
2025-12-11 21:29:23 +0300

    Task4

---
+++ Tyuiu.TsvetkovRA.Sprint6.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.TsvetkovRA.Sprint6.Task4.V28.Lib
+{
+    public class DataService : ISprint6Task4V28
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int x = stopValue - startValue + 1;
+            double[] res = new double[x];
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double y = Math.Round((Math.Cos(2 * i)) + ((Math.Sin(i)) / (i + 2.5)) + (2 * i), 2);
+                res[count] = y;
+                count++;
+
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint6.Task4.V28/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.TsvetkovRA.Sprint6.Task4.V28
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
